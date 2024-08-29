9b185d47390823101499358961374b386141a5a3
yashodhah <y.dominatz@gmail.com>
2024-08-29 22:51:12 +0800

    initial classes for SOLID

---
+++ src/main/java/dev/h/yashodha/design_playground/DesignPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package dev.h.yashodha.design_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPlaygroundApplication.class, args);
+	}
+
+}
